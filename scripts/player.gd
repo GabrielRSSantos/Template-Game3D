f81218ba18a422c67adc8efae1227e10d86450c3
Gabriel Santos <gabriel.santos@capgemini.com>
2026-01-29 19:23:52 -0300

    Cleaning code

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,11 +9,24 @@ const SENSITIVITY = 0.004
 @onready var raycast = $head/Camera3D/RayCast3D
 @export var is_first_person := false
 
+#Player Animation
+@onready var character_female_a: Node3D = $"character-female-a"
+@onready var animation_player: AnimationPlayer = $"character-female-a/AnimationPlayer"
+var is_attacking := false
+
 #BULLET
 @onready var gun_barrel = $head/coming_bullet
 @onready var aim = $Aim/AimMesh
 @onready var end_point_raycast: MeshInstance3D = $head/Camera3D/RayCast3D/bullet_position
 var bullet = load("res://scenes/bullet.tscn")
+
+#Player Interaction
+@onready var player_interaction: RayCast3D = $head/Camera3D/PlayerInteraction
+var can_search := false
+
+#Player Interaction TopDown
+@onready var player_interaction_top_down: Area3D = $PlayerInteraction
+var searching := false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -22,23 +35,27 @@ func _unhandled_input(event):
 	if event.is_action_pressed("camera_control"):
 		is_first_person = !is_first_person
 	if event is InputEventMouseMotion:
+		character_female_a.rotate_y(-event.relative.x * SENSITIVITY)
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(10))
-	if event.is_action_pressed("Teste"):
-		print("Teste pressed")
-		$"../Abandoned_House2/Casa/Puerta".rotate_y(90)
+	#if event.is_action_pressed("Teste"):
+		#print("Teste pressed")
+		#$"../Abandoned_House2/Casa/Puerta".rotate_y(90)
 
 func _process(delta: float) -> void:
 	gun_barrel.look_at(end_point_raycast.global_transform.origin)
+	#player_interaction_search_logic()
 	endpoint_raycast_update()
-	first_person_config()
+	
+	#Camera
+	#first_person_config()
 
 func _physics_process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		bullet_instance()
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		#bullet_instance()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("space") and is_on_floor():
@@ -54,7 +71,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	animation_character(direction)
+	
+	if can_search:
+		if Input.is_action_just_pressed("E"):
+			print("START SEARCHING")
+			searching = true
+	if searching:
+		player_search(delta)
 	move_and_slide()
 
 func first_person_config() -> void:
@@ -77,3 +101,43 @@ func endpoint_raycast_update() -> void:
 		end_point_raycast.global_transform.origin = raycast.get_collision_point()
 	else:
 		end_point_raycast.position = Vector3(-0.1, -0.7, -10)
+
+func player_interaction_search_logic() -> void:
+	if player_interaction.is_colliding() and player_interaction.get_collider() is Box:
+		can_search = true
+	else:
+		can_search = false
+
+func animation_character(direction) -> void:
+	if is_attacking:
+		return
+		
+	if not is_on_floor():
+		animation_player.play("jump")
+	elif direction and is_on_floor():
+		animation_player.play("walk")
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		is_attacking = true
+		animation_player.play("attack-melee-right")
+	else:
+		animation_player.play("idle")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "attack-melee-right":
+		is_attacking = false
+
+func _on_player_interaction_body_entered(body: Node3D) -> void:
+	can_search = check_is_box(body)
+	print(can_search)
+
+func _on_player_interaction_body_exited(body: Node3D) -> void:
+	can_search = check_is_box(body)
+	print(can_search)
+
+func check_is_box(box : Node3D) -> bool:
+	return box is Box
+	
+func player_search(delta) -> void:
+	print("Searching...")
+	$Sprite3D.visible = true
+	$SubViewport/ProgressBar.value += delta * 10
---
+++ scripts/player_top_down.gd
@@ -0,0 +1,111 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.004
+
+@onready var head = $Head
+@onready var camera = $Head/CameraHead
+@onready var raycast = $Head/RayCastBullet
+
+#Player Animation
+@onready var character_female_a: Node3D = $"character-female-a"
+@onready var animation_player: AnimationPlayer = $"character-female-a/AnimationPlayer"
+var is_attacking := false
+
+#BULLET
+@onready var bullet_spawn = $Head/BulletSpawn
+@onready var end_point_raycast: MeshInstance3D = $Head/RayCastBullet/BulletPosition
+var bullet = load("res://scenes/bullet.tscn")
+
+#Player Interaction
+@onready var sub_viewport: SubViewport = $SubViewport
+@onready var sprite_sub_viewport: Sprite3D = $SpriteViewPort
+@onready var progress_bar_searching: ProgressBar = $SubViewport/ProgressBar
+@onready var player_interaction_top_down: Area3D = $Head/PlayerInteraction
+
+var can_rotate := true
+var can_search := false
+var searching := false
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and can_rotate:
+		character_female_a.rotate_y(-event.relative.x * SENSITIVITY)
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		bullet_instance()
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	animation_character(direction)
+	player_search(delta)
+	move_and_slide()
+
+func bullet_instance() -> void:
+	var instance = bullet.instantiate()
+	instance.position = bullet_spawn.global_position
+	instance.transform.basis = bullet_spawn.global_transform.basis
+	get_parent().add_child(instance)
+
+func animation_character(direction) -> void:
+	if is_attacking:
+		return
+		
+	if not is_on_floor():
+		animation_player.play("jump")
+	elif direction and is_on_floor():
+		animation_player.play("walk")
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		is_attacking = true
+		animation_player.play("attack-melee-right")
+	else:
+		animation_player.play("idle")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "attack-melee-right":
+		is_attacking = false
+
+func _on_player_interaction_body_entered(body: Node3D) -> void:
+	can_search = check_is_box(body)
+
+func _on_player_interaction_body_exited(body: Node3D) -> void:
+	can_search = false
+
+func check_is_box(box : Node3D) -> bool:
+	return box is Box
+	
+func player_search(delta) -> void:
+	if can_search:
+		if Input.is_action_just_pressed("E"):
+			searching = true
+	else:
+		searching = false
+		
+	if searching:
+		can_rotate = false
+		sprite_sub_viewport.visible = true
+		progress_bar_searching.value += delta * 10
+	else:
+		can_rotate = true
+		sprite_sub_viewport.visible = false
+		progress_bar_searching.value = 0
